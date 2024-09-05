71fe8c4fa4fd6b579e12cac83d370000d3a5357c
yildirim-murat <muratyildirim.work@gmail.com>
2024-09-05 13:51:31 +0300

    Reload, devTools and some keystrokes were disable

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import './assets/css/App.css'
 import MainPage from "./pages/mainPage.jsx";
+import {useEffect} from "react";
 
 function App() {
 
+    useEffect(() => {
+        const handleBeforeUnload = (event)=>{
+            event.preventDefault();
+            event.returnValue = "Mesajım";
+        }
+
+        const disableDevTools=(event)=>{
+            if (event.key === "F12"){
+                event.preventDefault();
+            }
+
+            if ((event.ctrlKey && event.shiftKey && event.key === "I") || (event.ctrlKey && event.shiftKey && event.key === "C")){
+                event.preventDefault();
+            }
+        }
+
+        window.addEventListener("beforeunload", handleBeforeUnload);
+        window.addEventListener("keydown", disableDevTools);
+
+        return () => {
+            window.removeEventListener("beforeunload", handleBeforeUnload)
+            window.removeEventListener("keydown", disableDevTools)
+        };
+    }, []);
     return (
         <div style={{width: '100vw', height:"100vh", overflow:"hidden"}}>
             <MainPage/>
