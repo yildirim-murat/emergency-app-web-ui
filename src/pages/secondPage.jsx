7f40871ef1901c50d2685b8bde3971f1ac8a6899
yildirim-murat <muratyildirim.work@gmail.com>
2024-10-23 16:45:48 +0300

    end of day

--- src/pages/secondPage.jsx
+++ src/pages/secondPage.jsx
@@ -69,7 +69,7 @@ function SecondPage() {
             for (const name in departmentList) {
                 const data = await fetchData(name);
                 console.log("dataZZ: " + JSON.stringify(data))
-
+//Buradayım.
                 setSummaryData((prevState) => ({
                     ...prevState,
                     [name]: data,
